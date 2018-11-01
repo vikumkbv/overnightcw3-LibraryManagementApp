e8476bc231480565ea8f97f8663a9ad59d746cd3
vikumkbv <vikumkbv@esportlk.com>
2018-11-01 09:02:21 +0530

    WestminsterLibraryManager class to perform activities

---
+++ src/WestminsterLibraryManager.java
@@ -0,0 +1,42 @@
+public class WestminsterLibraryManager implements LibraryManager {
+
+    int libraryId;
+    String libraryName;
+    String location;
+
+    public WestminsterLibraryManager(int libraryId, String libraryName, String location) {
+        this.libraryId = libraryId;
+        this.libraryName = libraryName;
+        this.location = location;
+    }
+
+    @Override
+    public void addItem() {
+
+    }
+
+    @Override
+    public void deleteItem() {
+
+    }
+
+    @Override
+    public void borrowItem() {
+
+    }
+
+    @Override
+    public void returnItem() {
+
+    }
+
+    @Override
+    public void listItem() {
+
+    }
+
+    @Override
+    public void generateReport() {
+
+    }
+}
