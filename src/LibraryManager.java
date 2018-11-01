966baf5d7abfda1198f5ae339a59d2598bec6337
vikumkbv <vikumkbv@esportlk.com>
2018-11-01 09:01:05 +0530

    LibraryManager interface

---
+++ src/LibraryManager.java
@@ -0,0 +1,15 @@
+public interface LibraryManager {
+
+    void addItem();
+
+    void deleteItem();
+
+    void listItem();
+
+    void borrowItem();
+
+    void returnItem();
+
+    void generateReport();
+
+}
